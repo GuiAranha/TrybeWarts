067fa42939cb2895476ec3d01fdb84e05965bb20
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-11 19:03:22 -0300

    Requisito 19 - ok

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 window.onload = function(){
-    let buttonLogin = document.getElementById('buttonLogin');
+    let submitButton = document.getElementById('submit-btn');
+    let loginButton = document.getElementById('buttonLogin');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
+    let checkInfo = document.getElementById('agreement');
 
-    buttonLogin.addEventListener('click', function(event){
+    loginButton.addEventListener('click', function(event){
         if (email.value === 'tryber@teste.com' & password.value === '123456'){
             alert("Olá, Tryber!");
         }else{
             alert("Email ou senha inválidos.")
         }
     });
+    submitButton.disabled = true;
+    
+    checkInfo.addEventListener('click', function(event){
+        if (checkInfo.checked){
+            submitButton.disabled = false;
+        }else{
+            submitButton.disabled = true;
+        }
+    });
+
+
 }
