f67ad9331e07e16094a97653a5116059331ff404
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-01-11 21:28:27 -0300

    Requisito 21 - ok

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ window.onload = function(){
     let checkInfo = document.getElementById('agreement');
     let textarea = document.getElementById('textarea');
     let counter = document.getElementById('counter');
-
+    let form = document.getElementById('evaluation-form');
+    
+    function createLabel(text){
+        let label = document.createElement('label');
+        label.innerText = text;
+        form.appendChild(label);
+    }
+    
     loginButton.addEventListener('click', function(event){
         if (email.value === 'tryber@teste.com' & password.value === '123456'){
             alert("Olá, Tryber!");
@@ -14,8 +21,9 @@ window.onload = function(){
             alert("Email ou senha inválidos.")
         }
     });
+
     submitButton.disabled = true;
-    
+
     checkInfo.addEventListener('click', function(event){
         if (checkInfo.checked){
             submitButton.disabled = false;
@@ -26,6 +34,32 @@ window.onload = function(){
 
     textarea.addEventListener('input', function(event){
         counter.innerText = 500 - textarea.value.length;
+    });
+
+    submitButton.addEventListener('click', function(event){
+        event.preventDefault();
+        let inputName = document.getElementById('input-name');
+        let inputLastname = document.getElementById('input-lastname');
+        let house = document.getElementById('house');
+        let inputEmail = document.getElementById('input-email');
+        let family = document.querySelector('input[name="family"]:checked');
+        let rate = document.querySelector('input[name="rate"]:checked');
+        let content = document.querySelectorAll('input[name="conteudo"]:checked');
+        let contentChecked = "";
+        for (values of content){
+            contentChecked +=  values.value + ", ";
+        }
+        contentChecked = contentChecked.substring(0, contentChecked.length - 2);
+        form.innerHTML = "";
+        createLabel('Nome: ' + inputName.value + " " + inputLastname.value);
+        createLabel('Email: ' + inputEmail.value);
+        createLabel('Casa: ' + house.value);
+        createLabel('Família: ' + family.value);
+        createLabel('Matérias: ' + contentChecked);
+        createLabel('Avaliação: ' + rate.value);
+        createLabel('Observações: ' + textarea.value);
+        
+
     });
 
 }
